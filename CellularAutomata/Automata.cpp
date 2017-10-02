4cd40d68c4af72d272b47acc8410c09dff61c779
Zibe <jbaptiste.herve@gmail.com>
2017-10-02 09:49:37 +0200

    Double buffer.

--- CellularAutomata/Automata.cpp
+++ CellularAutomata/Automata.cpp
@@ -5,37 +5,51 @@ Automata::Automata(int _width, int _height)
 	: m_width(_width), m_height(_height), m_nbIterr(0)
 {
 	srand(time(0));
-	m_cells = std::vector<std::vector<Cell>>(_width);
+	m_buff1 = std::vector<std::vector<Cell>>(_width);
 	for (size_t i = 0; i < _width; i++)
 	{
-		m_cells[i] = std::vector<Cell>(_height);
+		m_buff1[i] = std::vector<Cell>(_height);
 	}
 
 	for (size_t i = 0; i < m_width; i++)
 	{
-		m_cells[i][0].setState(false);
-		m_cells[i][m_height - 1].setState(false);
+		m_buff1[i][0].setState(false);
+		m_buff1[i][m_height - 1].setState(false);
 	}
 	for (size_t i = 0; i < m_height; i++)
 	{
-		m_cells[0][i].setState(false);
-		m_cells[m_width - 1][i].setState(false);
+		m_buff1[0][i].setState(false);
+		m_buff1[m_width - 1][i].setState(false);
 	}
+
+	m_buff2 = m_buff1;
+
+	m_currBuff = std::unique_ptr<std::vector<std::vector<Cell>>>(&m_buff1);
 }
 
 void Automata::update()
 {
 	if (isProcessRunning())
 	{
-		std::vector<std::vector<Cell>> tmp = m_cells;
+		std::unique_ptr<std::vector<std::vector<Cell>>> oldBuff; ;
+		if (m_currBuff.get() == &m_buff1)
+		{
+			oldBuff = std::unique_ptr<std::vector<std::vector<Cell>>>(&m_buff2);
+		}
+		else
+		{
+			oldBuff = std::unique_ptr<std::vector<std::vector<Cell>>>(&m_buff1);
+		}
+		m_currBuff.swap(oldBuff);
+
 		for (int i = 0; i < m_width; i++)
 		{
 			for (int j = 0; j < m_height; j++)
 			{
 
 				//We decrease the max count in order to consider tiles out of bounds like walls.
-				int count = m_cells[i][j].getState() ? 0 : 1;
-				count += GetCellsInRangeState(1, i, j);
+				int count = 9 - (m_buff1[i][j].getState() ? 1 : 0);
+				count -= GetCellsInRangeState(1, i, j, oldBuff.get());
 
 				//Game of life
 				/*if (count != 2)
@@ -45,67 +59,59 @@ void Automata::update()
 
 				if (m_nbIterr < 4 && count < 5)
 				{
-					count += GetCellsInRangeState(2, i, j) ;
-					tmp[i][j].setState(count > 2);
+					count += 16 - GetCellsInRangeState(2, i, j, oldBuff.get()) ;
+					(*m_currBuff)[i][j].setState(count > 2);
 				}
 				else
 				{
-					tmp[i][j].setState(count < 5);
+					(*m_currBuff)[i][j].setState(count < 5);
 				}
 
 			}
 		}
 		++m_nbIterr;
-		m_cells = tmp;
+		oldBuff.release();
 	}
 }
 
-int Automata::GetCellsInRangeState(int _range, int _column, int _row) const
+int Automata::GetCellsInRangeState(int _range, int _column, int _row, std::vector<std::vector<Cell>> *_buff) const
 {
 	int count = 0;
 
+	int lowerBound = _row - _range;
+	int upperBound = _row + _range;
 	for (int i = _column - _range; i <= _column + _range; i++)
 	{
-		int j = _row - _range;
-		int k = _row + _range;
 
 		if (i >= 0 && i < m_width)
 		{
-			if (j < 0 || j >= m_height || !m_cells[i][j].getState())
+			if (lowerBound >= 0 && lowerBound < m_height && (*_buff)[i][lowerBound].getState())
 			{
 				++count;
 			}
 
-			if (k < 0 || k >= m_height || !m_cells[i][k].getState())
+			if (upperBound >= 0 && upperBound < m_height && (*_buff)[i][upperBound].getState())
 			{
 				++count;
 			}
-		}
-		else
-		{
-			count += 2;
 		}
 	}
 
+	lowerBound = _column - _range;
+	upperBound = _column + _range;
 	for (size_t i = _row - _range + 1; i < _row + _range; i++)
 	{
-		int j = _column - _range;
-		int k = _column + _range;
 
 		if (i >= 0 && i < m_height)
 		{
-			if (j < 0 || j >= m_width || !m_cells[j][i].getState())
+			if (lowerBound >= 0 && lowerBound < m_width && (*_buff)[lowerBound][i].getState())
 			{
 				++count;
 			}
-			if (k < 0 || k >= m_width || !m_cells[k][i].getState())
+			if (upperBound >= 0 && upperBound < m_width && (*_buff)[upperBound][i].getState())
 			{
 				++count;
 			}
-		}
-		else
-		{
-			count += 2;
 		}
 	}
 
--- CellularAutomata/Automata.h
+++ CellularAutomata/Automata.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include <memory>
+
 #include "Cell.h"
 
 class Automata
@@ -11,8 +13,7 @@ public:
 	void update();
 
 
-	const std::vector<std::vector<Cell>>* getCells() const { return &m_cells; };
-	void setCells(std::vector<std::vector<Cell>>& _cells) { m_cells = _cells; }
+	const std::vector<std::vector<Cell>> *getCells() const { return m_currBuff.get(); };
 
 	int getWidth() const { return m_width; }
 	int getHeight() const { return m_height; }
@@ -21,9 +22,12 @@ public:
 
 private:
 
-	int GetCellsInRangeState(int _range, int _column, int _line) const;
+	int GetCellsInRangeState(int _range, int _column, int _line, std::vector<std::vector<Cell>> *_buff) const;
 
-	std::vector<std::vector<Cell>> m_cells;
+	std::vector<std::vector<Cell>> m_buff1;
+	std::vector<std::vector<Cell>> m_buff2;
+	std::unique_ptr<std::vector<std::vector<Cell>>> m_currBuff;
+
 	int m_width;
 	int m_height;
 
